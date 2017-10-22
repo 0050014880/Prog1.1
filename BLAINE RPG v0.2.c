d792321e95b8f68d49b4e9d82f731a30b469909f
0050014880 <32278758+0050014880@users.noreply.github.com>
2017-10-22 03:20:31 -0200

    Update BLAINE RPG v0.2.c
    mexi um pouco no programa, coloquei uma luta com o mostro

--- BLAINE RPG v0.2.c
+++ BLAINE RPG v0.2.c
@@ -7,25 +7,24 @@ David Gagliano
 #include <unistd.h>
 #include <time.h> //Tentativa de usar um comando para esperar.
 #include <stdlib.h>
-#include <locale.h> //necess·rio para usar setlocale.
+#include <locale.h> //necess√°rio para usar setlocale.
 
 /*
 m1=Lestrigones;
 m2=Godizika;
 m3=Ness;
-
-N„o estou conseguindo colocar os nomes dos monstros
+N√£o estou conseguindo colocar os nomes dos monstros
 */
-// printf("A localidade corrente agora È %s \n",setlocale(LC_ALL,""));
+// printf("A localidade corrente agora √© %s \n",setlocale(LC_ALL,""));
 
 int main(){
 char nome[15], m1,m2[15],m3[15];
 int ataque, vida, classe,decisao1,decisao2;
 
 
-printf("",setlocale(LC_ALL,"")); //Para definir a lingua, mas n„o sei usar.
+printf("",setlocale(LC_ALL,"")); //Para definir a lingua, mas n√£o sei usar.
 
-printf("Bem vindo!\nEsse È o mundo imaginario.\n Nosso herÛi vive no vilarejo alfa e ir· para o castelo do demonio.\n"), m3;
+printf("Bem vindo!\nEsse √© o mundo imaginario.\n Nosso her√≥i vive no vilarejo alfa e ir√° para o castelo do demonio.\n"), m3;
 printf("Entre com o nome do seu personagem:\n");
 
 scanf("%s", &nome);
@@ -44,34 +43,66 @@ printf("----------------------------------------------");
 printf("\n%s saiu do vilarejo em busca de sua jornada...", nome);
 
 printf("\n");//Simples pulador de linha
-system("pause");//AÁ„o para continuar a historia devagar
+system("pause");//A√ß√£o para continuar a historia devagar
 
 printf("\nSeguindo seu caminho, andando tranquilamente com seus equipamentos, %s avista um monstro.\n", nome);
-printf("Com extremo receio, nosso herÛi se vÍ impotente diante do monstro.\n");
-printf("Decis„o:\n Ir de encontro com o monstro.(1) | Pegar um contorno.(2)\n");
+printf("Com extremo receio, nosso her√≥i se v√™ impotente diante do monstro.\n");
+printf("Decis√£o:\n Ir de encontro com o monstro.(2) | Pegar um contorno.(1)\n");
 scanf("%d", &decisao1);
 
-if (decisao1 == 1) goto MORREU; //Termos de condiÁ„o
-else (printf("VocÍ pegou um caminho de terra em direÁ„o a montanha.")); //Termos de condiÁ„o
+if (decisao1!=1){
+	
+	
+//N√£o esta funcionando bem do jeito que eu queria	
+while (decisao1 == 1) 
+printf("\nO Mostro atacou, porem voce defendeu com o escudo");
+	printf("\nPredeu 5 vida");
+	vida=vida-5;
+	printf("\ntotal ###vida: %d ####",vida);
+
+
+printf("\nVoce esta de cara com o mostro... Deseja ataca-lo? (1)sim ou (2)nao: "); //Termos de condi√ß√£o
+scanf("%d",&decisao1);
+
+printf("\nVoce ganhou 10 vida");
+vida=vida+10;
+printf("\ntotal ###vida: %d ####",vida);
+printf("\nDeseja ataca-lo novamente ou usar o escudo? (1)atacar ou (2)escudo:"); 
+scanf("%d",&decisao1);
+printf("\nO mostro te atacou tambem e voce perdeu 10 vida");
+vida=vida-10;
+printf("\ntotal ###vida: %d ####",vida);
+printf("\nDeseja ataca-lo novamente ou usar o escudo? (1)atacar ou (2)escudo:"); 
+scanf("%d",&decisao1);
+printf("\nVenceu o mostro ganhou 20 vida"); 
+vida=vida+20;
+printf("\ntotal ###vida: %d ####",vida);
+
+}
+
+
+else 
+
+printf("Voc√™ pegou um caminho de terra em dire√ß√£o a montanha."); //Termos de condi√ß√£o
 
 printf("\n");
 system("pause");
 
-printf("\nSeguindo o caminho de terra. Ao olhar para esquerda, È possÌvel ver um vilarejo desconhecido.");
+printf("\nSeguindo o caminho de terra. Ao olhar para esquerda, √© poss√≠vel ver um vilarejo desconhecido.");
 printf("Deseja ir no vilarejo?");
-printf("Decis„o:\n Ir ao vilarejo.(1) | Continuar seguindo em frente.(2)\n");
+printf("Decis√£o:\n Ir ao vilarejo.(1) | Continuar seguindo em frente.(2)\n");
 scanf("%d", &decisao1);
 
 if(decisao1 == 1) goto vilarejoestranho; //Mapa do vilarejo
 
-vilarejoestranho0: printf("\nHavia alguma coisa estranha naquele lugar...\nAinda bem que evitamos ir l·.\n");
+vilarejoestranho0: printf("\nHavia alguma coisa estranha naquele lugar...\nAinda bem que evitamos ir l√°.\n");
 
 printf("\n");
 system("pause");
 printf("\n");//Simples pulador de linha
 
-printf("A estrada de terra acabou.\nLogo a frente h· uma montanha que parece cortar um bom caminho.\nNa direita h· uma mans„o de um duque, podemos tentar pedir estadia.\n");
-printf("Decis„o:\nSubir a montanha(1) | Ir para mans„o(2)\n");
+printf("A estrada de terra acabou.\nLogo a frente h√° uma montanha que parece cortar um bom caminho.\nNa direita h√° uma mans√£o de um duque, podemos tentar pedir estadia.\n");
+printf("Decis√£o:\nSubir a montanha(1) | Ir para mans√£o(2)\n");
 scanf("%d", &decisao1);
 
 if(decisao1 ==2) goto mansaodomal;
@@ -93,35 +124,35 @@ printf("O tempo fechou e estava prestes a chover\n");
 
 goto final;
 //--------------------------------CENARIOS
-vilarejoestranho:  printf("Chegando perto do vilarejo, nosso herÛi sente um cheiro estranho.\n… possivel que seja de cad·ver.");
+vilarejoestranho:  printf("Chegando perto do vilarejo, nosso her√≥i sente um cheiro estranho.\n√â possivel que seja de cad√°ver.");
 
 printf("\n");//Simples pulador de linha
-system("pause");//AÁ„o para continuar a historia devagar
+system("pause");//A√ß√£o para continuar a historia devagar
 
-printf("… possivÈl que tenha pistas do ocorrido dentro do vilarejo, mas muito arriscado...");
-printf("Decis„o:\n Entrar no vilarejo mesmo assim(1) | Dar meia volta e continuar o caminho de onde parou (2)");
+printf("√â possiv√©l que tenha pistas do ocorrido dentro do vilarejo, mas muito arriscado...");
+printf("Decis√£o:\n Entrar no vilarejo mesmo assim(1) | Dar meia volta e continuar o caminho de onde parou (2)");
 scanf("%d", &decisao2);
 
 if(decisao2 ==2) goto vilarejoestranho0;
-else (printf("VocÍ foi avisado...\n"));
+else (printf("Voc√™ foi avisado...\n"));
  goto MORREU;
 //----------------------------------------------------------------------------------------------
-mansaodomal: printf("Chegando perto da mans„o, aparentemente normal, ele para e reflete.\n");
-printf("Nosso herÛi fica observando se h· alguem morando na mans„o, mas aparenta estar abandonada.\n");
-printf("\tDecis„o:\n Entrar na mansao(1) | Voltar e subir a montanha(2)\n");
+mansaodomal: printf("Chegando perto da mans√£o, aparentemente normal, ele para e reflete.\n");
+printf("Nosso her√≥i fica observando se h√° alguem morando na mans√£o, mas aparenta estar abandonada.\n");
+printf("\tDecis√£o:\n Entrar na mansao(1) | Voltar e subir a montanha(2)\n");
 scanf("%d", &decisao1);
 	
 	if(decisao1 == 2) goto mansaodomal0;
-printf("Nosso herÛi abriu a porta e adentrou ao local\n");
-printf("ComeÁou a andar pelos cÙmodos e achou um local bom para dormir\n");
+printf("Nosso her√≥i abriu a porta e adentrou ao local\n");
+printf("Come√ßou a andar pelos c√¥modos e achou um local bom para dormir\n");
 printf("Parece que vai chover essa noite!\n");
 //----------------------------------------------------------------------------------------------
 //--------------------------------CENARIOS
 
 //Finais do jogoo
 final:
-MORREU: printf("\n\n\t\t VocÍ morreu...\n\n");
-VIVEU: printf("\n\n\t\tEspere o prÛximo capÌtulo. |:D|\n\n");
+MORREU: printf("\n\n\t\t Voc√™ morreu...\n\n");
+VIVEU: printf("\n\n\t\tEspere o pr√≥ximo cap√≠tulo. |:D|\n\n");
 
 system("pause");
 return 0;
